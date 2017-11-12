ffeecd0ccbea843c665d8a9826e051012239d3ae
Ahmet Kılınç <ahmetkilinc3899@gmail.com>
2017-11-12 23:56:04 +0300

    Sabir gider activity si eklendi. xml tasarımı yapıldı. toplam gider hesabı yapıldı. toplam gider ana sayfaya intent ile gönderildi.

--- app/src/main/java/com/gobletsoft/gunlukharcamam/gunlukHarcamaLimitim.java
+++ app/src/main/java/com/gobletsoft/gunlukharcamam/gunlukHarcamaLimitim.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class gunlukHarcamaLimitim extends AppCompatActivity {
+
+    public Double toplamGider = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public class gunlukHarcamaLimitim extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_gunluk_harcama_limitim);
+
+        TextView tv = (TextView) findViewById(R.id.textViewToplamGider);
+
+        //sabit gider sayfasından hesaplanan değeri geri almak.
+
+        Intent intent1 = this.getIntent();
+        toplamGider = intent1.getDoubleExtra("toplamGider", 0.0);
+        Toast.makeText(getApplicationContext(), "toplam: " + toplamGider, Toast.LENGTH_LONG).show();
+        tv.setText(toplamGider.toString());
 
         Button btnSabitGiderEkle = (Button) findViewById(R.id.buttonSabitGiderEkle);
 
